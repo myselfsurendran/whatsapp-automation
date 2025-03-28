b78461c62a2f5e01e4937c49a9620488f7e8c32d
myselfsurendran <myselfsurendran@gmail.com>
2025-03-28 09:40:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas as pd
 from datetime import datetime
 import subprocess
 from config import (
-    DATA_FILE_PATH,
+    DATA_FILE_PATH_ENGLISH,
+    DATA_FILE_PATH_TAMIL,
+    GROUP_NAME,
     MESSAGE_FILE,
     DAY_PLACE_MAPPING_FILE_PATH 
 )
@@ -12,7 +14,7 @@ def get_place_for_today():
     today_date = datetime.now().strftime('%Y-%m-%d')
     try:
         mapping_df = pd.read_excel(DAY_PLACE_MAPPING_FILE_PATH)  
-        print(mapping_df)
+        #print(mapping_df)
         place_row = mapping_df[mapping_df['Date'] == today_date] 
         print(place_row )
         if not place_row.empty:
@@ -30,32 +32,47 @@ def get_place_for_today():
         return None
 
 def process_and_send_data():
-    place_name = get_place_for_today()
-    if not place_name:
-        return
+    with open('groups.txt', 'r', encoding='utf8') as f:
+        groups = [line.strip() for line in f]
 
-    try:
-        df = pd.read_excel(DATA_FILE_PATH) 
+    for group in groups:
+        print(f'Processing for group: {group}')
 
-        place_data = df[df.iloc[:, 0] == place_name].iloc[:, 1].iloc[0]
+        place_name = get_place_for_today()
+        if not place_name:
+            return
+    
+        try:
+            df_english = pd.read_excel(DATA_FILE_PATH_ENGLISH)  # Or pd.read_csv
 
-        print(f"Data found for {place_name}: {place_data}")
+            df_tamil = pd.read_excel(DATA_FILE_PATH_TAMIL)
+            print(group)
 
-        with open(MESSAGE_FILE, 'w', encoding='utf8') as f:
-            f.write(place_data)
+            if group == 'Zi Tours - English - Test':
+                place_data = df_english[df_english.iloc[:, 0] == place_name].iloc[:, 1].iloc[0]
+            else:
+                place_data = df_tamil[df_tamil.iloc[:, 0] == place_name].iloc[:, 1].iloc[0]
 
-        print("Data written to msg.txt")
+            print(f"Data found for {place_name}")
 
-        subprocess.run(['python3', 'whatsapp-automation/whatsapp_script.py', place_name])
+            with open(MESSAGE_FILE, 'w', encoding='utf8') as f:
+                f.write(place_data)
+            
+            with open(GROUP_NAME, 'w', encoding='utf8') as f:
+                f.write(group)
+
+            print("Data written to msg.txt")
+
+            subprocess.run(['python3', 'whatsapp-automation/whatsapp_script.py', place_name])
         
-        print("WhatsApp automation script triggered.")
+            print("WhatsApp automation script triggered.")
 
-    except FileNotFoundError:
-        print(f"Error: Data file '{DATA_FILE_PATH}' not found.")
-    except KeyError:
-        print(f"Error: Place '{place_name}' not found in the data.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+        except FileNotFoundError:
+            print(f"Error: Data file '{DATA_FILE_PATH_ENGLISH} or {DATA_FILE_PATH_TAMIL}' not found.")
+        except KeyError:
+            print(f"Error: Place '{place_name}' not found in the data.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     process_and_send_data()
